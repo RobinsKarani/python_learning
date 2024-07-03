fa8afecf74125923750bb6f147729d1ca3099d91
Robins K.T <karanithiaine4@gmail.com>
2024-07-03 14:29:21 +0300

    day 2,basics of strings..to be reviewed

--- error.py
+++
@@ -1,5 +0,0 @@
-foko =4
-try:
-    foko = 10 / 0
-except ZeroDivisionError as e:
-    print(e)  # division by zero
---
+++ test.py
@@ -0,0 +1,10 @@
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
+print(f'({a} * {b}) **2 ={a*b*a*b}')
