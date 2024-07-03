e44b44bb0e79a1d1fbfdb1ecf9922c81f81f24ea
Robins K.T <karanithiaine4@gmail.com>
2024-07-03 18:42:18 +0300

    calculating area of a  circle

---
+++ 30_days_robins/day2/circle_area.py
@@ -0,0 +1,7 @@
+
+# area of a circle
+radius = input(f"Enter the radius of the circle: ")
+radius = int(radius)
+area = 3.14 * radius ** 2
+area = float(area)
+print(f"The area of the circle is {area}")
