e4077765aba32aabd68b1c7b00748a37ee63c919
GooNinja <karanithiaine4@gmail.com>
2024-07-04 12:40:43 +0300

    if statements basics

---
+++ 30_days_robins/day3/maths.py
@@ -0,0 +1,180 @@
+import math
+# x = 3.14
+# y = 4
+# z = 5
+# # # esult = round(x)
+# # # result = abs (y)
+# # # result = pow(y, 3)
+# result = max(x, y, z)
+# # print(result)
+#
+# print(math.sqrt(result))
+# print(math.floor(result))
+# print(math.ceil(result))
+
+# circumference of  circle
+#2pir
+
+radius = input(f'enter the radius of the circle: ')
+radius = float(radius)
+circumference = 2 * math.pi * radius
+circumference = round(circumference, 2)  # rounding to two decimal places
+circ = circumference
+
+
+print(f'The circumference of the circle is {circ} cm long ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
---
+++ 30_days_robins/day3/tuples/tuples.py
@@ -0,0 +1,4 @@
+# syntax
+tpl = ('item1', 'item2', 'item3')
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print(len(fruits))
