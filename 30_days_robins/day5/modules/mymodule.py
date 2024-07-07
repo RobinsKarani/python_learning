a96eea33afc633e08e5a2b9cf8cf1f0182b9cab7
RobinsKarani <karanithiaine4@gmail.com>
2024-07-07 17:24:15 +0300

    self made module

--- 30_days_robins/day5/modules/mymodule.py
+++ 30_days_robins/day5/modules/mymodule.py
@@ -4,8 +4,4 @@ def greet(name):
 
 def add(a, b):
     return a + b
-# main.py
-import mymodule
 
-print(mymodule.greet("Alice"))  # Prints: Hello, Alice!
-print(mymodule.add(3, 5))       # Prints: 8
