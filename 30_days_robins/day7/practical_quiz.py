ea4b1011a5b2fec1ce72c0df1d0a8d17c3e30283
RobinsKarani <karanithiaine4@gmail.com>
2024-07-08 23:33:56 +0300

    practical quiz

---
+++ 30_days_robins/day7/practical_quiz.py
@@ -0,0 +1,9 @@
+rows = 3
+column = 5
+symbol = input('Enter the symbol you want to use to form your rectangle: ')
+
+for x in range(rows):
+    for y in range(column):
+        print(symbol, end="")
+    print()
+
