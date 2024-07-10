d8bb2006d967689cbea8391e472a2bf1ea88bfc5
RobinsKarani <karanithiaine4@gmail.com>
2024-07-10 23:03:05 +0300

    function as a return value

---
+++ day9/function_as_return_value.py
@@ -0,0 +1,30 @@
+def square(x):  # a square function
+    return x ** 2
+
+
+def cube(x):  # a cube function
+    return x ** 3
+
+
+def absolute(x):  # an absolute value function
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+
+def higher_order_function(_type):  # a higher order function returning a function
+    if _type == 'square':
+        return square
+    elif _type == 'cube':
+        return cube
+    elif _type == 'absolute':
+        return absolute
+
+
+result = higher_order_function('square')
+print(result(3))  # 9
+result = higher_order_function('cube')
+print(result(3))  # 27
+result = higher_order_function('absolute')
+print(result(-3))  # 3
