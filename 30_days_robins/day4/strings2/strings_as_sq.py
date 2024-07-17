36f353c7b55881be10f864e723e60136953a01cf
RobinsKarani <karanithiaine4@gmail.com>
2024-07-17 09:42:01 +0300

    str formatting

---
+++ 30_days_robins/day4/strings2/strings_as_sq.py
@@ -0,0 +1,9 @@
+# strings as sequence of characters
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
