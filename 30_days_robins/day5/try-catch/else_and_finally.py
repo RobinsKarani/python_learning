22376e8f80d823e31d8b85191c81c15c6545b6ad
RobinsKarani <karanithiaine4@gmail.com>
2024-07-31 16:49:35 +0300

    else and finally

---
+++ 30_days_robins/day5/try-catch/else_and_finally.py
@@ -0,0 +1,5 @@
+"""
+Using Else and Finally
+The else block can be used to run code if no exceptions are raised,
+and the finally block can be used to run code regardless of whether an exception was raised.
+"""
